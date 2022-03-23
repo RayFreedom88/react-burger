c552adb6ed11498af3274827c28755855ebc0437
RayFreedom88 <captain.musin@yandex.ru>
2022-03-23 16:26:36 +0200

    fix: переименует, изменит формат с tsx на jsx и удалит ненужные файлы

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,21 +1,19 @@
-// import React from 'react';
 import AppHeader from '../app-header/app-header';
-import './App.css';
+import AppStyles from './app.module.css';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import { data } from "../../utils/data";
 
 function App() {
   const ingredients = [...data];
-  console.log(ingredients);
   
   return (
     <div className="App">
       <AppHeader />
-      <main className='main'>
+      <main className={AppStyles.app__main}>
           <h1 className="visually-hidden">Главная страница сайта Stellar Burgers</h1>
-          <section className='main__section'>
-            <BurgerIngredients />
+          <section className={AppStyles.app__section}>
+            <BurgerIngredients items={ingredients} />
             <BurgerConstructor />
           </section>
       </main>
