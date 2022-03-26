b61c3455022990a64ebf908f5508480161993031
RayFreedom88 <captain.musin@yandex.ru>
2022-03-26 22:31:06 +0200

    fix: исправить разметку и стили BurgerConstructor

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -8,19 +8,19 @@ function BurgerConstructor () {
         <div className={BurgerConstructorStyles.burgerConstructor}>
             <h2 className="visually-hidden">Лента заказов</h2>
 
-            <div className={BurgerConstructorStyles.burgerConstructor__scrollWrapper}>
-                <ul className={BurgerConstructorStyles.burgerConstructor__list}>
-                    <li className={BurgerConstructorStyles.burgerConstructor__item}>
-                        <ConstructorElement
-                            type="top"
-                            isLocked={true}
-                            text="Краторная булка N-200i (верх)"
-                            price={200}
-                            thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-                        />
-                    </li>
+            <div className={BurgerConstructorStyles.top}>
+                <ConstructorElement
+                    type="top"
+                    isLocked={true}
+                    text="Краторная булка N-200i (верх)"
+                    price={200}
+                    thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
+                />
+            </div>
 
-                    <li className={BurgerConstructorStyles.burgerConstructor__item}>
+            <div className={BurgerConstructorStyles.scrollWrapper}>
+                <ul className={BurgerConstructorStyles.list}>
+                    <li className={BurgerConstructorStyles.item}>
                         <DragIcon type="primary" />
 
                         <ConstructorElement
@@ -30,7 +30,7 @@ function BurgerConstructor () {
                         />
                     </li>
 
-                    <li className={BurgerConstructorStyles.burgerConstructor__item}>
+                    <li className={BurgerConstructorStyles.item}>
                         <DragIcon type="primary" />
 
                         <ConstructorElement
@@ -40,7 +40,7 @@ function BurgerConstructor () {
                         />
                     </li>
 
-                    <li className={BurgerConstructorStyles.burgerConstructor__item}>
+                    <li className={BurgerConstructorStyles.item}>
                         <DragIcon type="primary" />
 
                         <ConstructorElement
@@ -50,7 +50,7 @@ function BurgerConstructor () {
                         />
                     </li>
 
-                    <li className={BurgerConstructorStyles.burgerConstructor__item}>
+                    <li className={BurgerConstructorStyles.item}>
                         <DragIcon type="primary" />
 
                         <ConstructorElement
@@ -60,7 +60,17 @@ function BurgerConstructor () {
                         />
                     </li>
 
-                    <li className={BurgerConstructorStyles.burgerConstructor__item}>
+                    <li className={BurgerConstructorStyles.item}>
+                        <DragIcon type="primary" />
+
+                        <ConstructorElement
+                            text="Краторная булка N-200i (верх)"
+                            price={50}
+                            thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
+                        />
+                    </li>
+
+                    <li className={BurgerConstructorStyles.item}>
                         <DragIcon type="primary" />
 
                         <ConstructorElement
@@ -70,7 +80,7 @@ function BurgerConstructor () {
                         />
                     </li>
                     
-                    <li className={BurgerConstructorStyles.burgerConstructor__item}>
+                    <li className={BurgerConstructorStyles.item}>
                         <DragIcon type="primary" />
 
                         <ConstructorElement
@@ -80,29 +90,29 @@ function BurgerConstructor () {
                         />
                     </li>
 
-                    <li className={BurgerConstructorStyles.burgerConstructor__item}>
+                    <li className={BurgerConstructorStyles.item}>
                         <DragIcon type="primary" />
 
                         <ConstructorElement
                             text="Краторная булка N-200i (верх)"
                             price={50}
-                            thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-                        />
-                    </li>
-
-                    <li className={BurgerConstructorStyles.burgerConstructor__item}>
-                        <ConstructorElement
-                            type="bottom"
-                            isLocked={true}
-                            text="Краторная булка N-200i (низ)"
-                            price={200}
                             thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
                         />
                     </li>
                 </ul>
             </div>
 
-            <div className={BurgerConstructorStyles.burgerConstructor__cost}>
+            <div className={BurgerConstructorStyles.bottom}>
+                <ConstructorElement
+                    type="bottom"
+                    isLocked={true}
+                    text="Краторная булка N-200i (низ)"
+                    price={200}
+                    thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
+                />
+            </div>
+
+            <div className={BurgerConstructorStyles.cost}>
                 <p className="text text_type_digits-medium mt-1 mr-5 mb-1 pr-5">
                     <span>600</span>&nbsp;
                     <CurrencyIcon type="primary" />
