93e86e0a22202c966818b14b7f1d37585a9bc4a9
RayFreedom88 <captain.musin@yandex.ru>
2022-03-27 03:31:18 +0200

    feat: добавить в PropsType описание структуры объекта

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -61,8 +61,23 @@ function BurgerConstructor ({items}) {
     );
 }
 
+const itemPropTypes = PropTypes.shape({
+    calories: PropTypes.number.isRequired,
+    carbohydrates: PropTypes.number.isRequired,
+    fat: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    image_large: PropTypes.string.isRequired,
+    image_mobile: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    proteins: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
+    __v: PropTypes.number.isRequired,
+    _id: PropTypes.string.isRequired
+  });
+
 BurgerConstructor.propTypes = {
-    items: PropTypes.arrayOf(PropTypes.object.isRequired)
+    items: PropTypes.arrayOf(itemPropTypes.isRequired)
 };
 
 export default BurgerConstructor;
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -97,8 +97,23 @@ function BurgerIngredients({items}) {
     );
 }
 
+const itemPropTypes = PropTypes.shape({
+    calories: PropTypes.number.isRequired,
+    carbohydrates: PropTypes.number.isRequired,
+    fat: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    image_large: PropTypes.string.isRequired,
+    image_mobile: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    proteins: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
+    __v: PropTypes.number.isRequired,
+    _id: PropTypes.string.isRequired
+  });
+
 BurgerIngredients.propTypes = {
-   items: PropTypes.arrayOf(PropTypes.object.isRequired)
+   items: PropTypes.arrayOf(itemPropTypes.isRequired)
 };
 
 export default BurgerIngredients;
